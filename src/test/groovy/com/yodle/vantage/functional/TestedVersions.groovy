e872fa697d56dfe0b86cf9d23d93f0cb15636ff2
dkesler <david.kesler@mlb.com>
2017-07-17 21:25:24 -0400

    generate task no longer removes configurations from project

--- src/test/groovy/com/yodle/vantage/functional/TestedVersions.groovy
+++ src/test/groovy/com/yodle/vantage/functional/TestedVersions.groovy
@@ -17,5 +17,5 @@ package com.yodle.vantage.functional
 
 class TestedVersions {
   private TestedVersions(){}
-  public static final List<String> GRADLE_VERSIONS = ['2.8', '2.9', '2.10', '2.11', '2.12', '2.13', '2.14.1', '3.0']
+  public static final List<String> GRADLE_VERSIONS = ['2.8', '2.9', '2.10', '2.11', '2.12', '2.13', '2.14.1', '3.0', '3.1', '3.2', '3.3', '3.4']
 }
